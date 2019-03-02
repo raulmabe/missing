10245deea8937db458400bb49963eb7b2c355d69
Rahuvich <raulmateob@gmail.com>
2019-03-02 15:42:08 +0100

    dummy update

--- lib/redux/auth/auth_middleware.dart
+++ lib/redux/auth/auth_middleware.dart
@@ -9,8 +9,17 @@ import '../actions.dart';
 import '../../models/appState.dart';
 import 'package:built_collection/built_collection.dart';
 import './auth_actions.dart';
+import 'package:http/http.dart' as http;
 
 Future<bool> checkIfUserExists(String email, String password) async{
+  fetchData();
   await Future.delayed(Duration(seconds: 4));
   return true;
 }
+
+void fetchData() async {
+    final response = await http.get('http://192.168.43.203:8888/fetch_data.php');
+    if (response.statusCode == 200) {
+        print(json.decode(response.body));
+    } else print("Error: ${response.statusCode}");
+  }
